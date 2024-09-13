be6ddc5b73fd46f03fb35c906a9d7f899947e057
y <>
2024-09-13 21:46:18 +0900

    project作成

---
+++ ActualMachineTest/ActualMachineTest/ActualMachineTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActualMachineTestApp.swift
+//  ActualMachineTest
+//
+//  Created by yohei on 2024/09/13.
+//
+
+import SwiftUI
+
+@main
+struct ActualMachineTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ActualMachineTest/ActualMachineTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ActualMachineTest
+//
+//  Created by yohei on 2024/09/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
